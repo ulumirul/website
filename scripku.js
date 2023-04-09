1720097b40356c80054c309764a78c063f60d689
ulumirul <126856759+ulumirul@users.noreply.github.com>
2023-04-09 21:30:13 +0700

    Add files via upload

---
+++ scripku.js
@@ -0,0 +1,21 @@
+$('.icon-ku').click(function(){
+    // $(this).toggleClass("sit");
+    $('.sildebar2').toggleClass("s2");
+    $('.sildebar').toggleClass("s1");
+    $('.sildebar3').toggleClass("s3");
+    $('.w1').toggleClass("m1");
+    $('.log_profil').toggleClass("x2");
+    $('.isi').toggleClass("c1");
+    // $('.oto').toggleClass("v1");
+});
+$('.popi').click(function(){
+    $('.log_profil').toggleClass("x1");
+})
+$('.rut').click(function(){
+    $('nav ul .yut').toggleClass("show");
+    $('nav ul .kort1').toggleClass("turu");
+});
+$('.rit').click(function(){
+    $('nav ul .yit').toggleClass("show2");
+    $('nav ul .kort').toggleClass("turu2");
+});
